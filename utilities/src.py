db482099a38a63ee413974f364775796186dd090
Alessandro Di Piano <aledip2014@gmail.com>
2026-03-20 15:11:34 +0100

    small chane in the extracting function

--- utilities/src.py
+++ utilities/src.py
@@ -10,18 +10,18 @@ def build_df(root):
         3: "low_amplitude"
     }
 
+    sample_id = 0
+
     for subject_dir in root.glob("s*"):
         for exercise_dir in subject_dir.glob("e*"):
             times_path = exercise_dir / "template_times.txt"
-            if not times_path.exists():
-                continue
+            
 
             template_times = pd.read_csv(times_path, sep=";")
 
             for unit_dir in exercise_dir.glob("u*"):
                 file_path = unit_dir / "template_session.txt"
-                if not file_path.exists():
-                    continue
+                
 
                 df = pd.read_csv(file_path, sep=";")
                 df["subject"] = subject_dir.name
@@ -30,13 +30,21 @@ def build_df(root):
             
 
                 for _, row in template_times.iterrows():
-                    start = row["start"]   # replace with actual column names
+                    start = row["start"]   
                     end = row["end"]
                     exec_type = row["execution type"]
 
-                    segment = df[(df["time index"] >= start) & (df["time index"] <= end)].copy()
+                    segment = df[
+                        (df["time index"] >= start) &
+                        (df["time index"] <= end)
+                    ].copy()
+
                     segment["execution_type"] = label_map[exec_type]
-                    templates.append(segment) 
-    templates_df = pd.concat(templates, ignore_index=True)
+                    segment["sample_id"] = sample_id
+
+                    templates.append(segment)
+
+                    sample_id += 1   
+                    templates_df = pd.concat(templates, ignore_index=True)
 
     return templates_df
